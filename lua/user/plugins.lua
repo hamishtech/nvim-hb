85df7061298ba370f405b345659c06edf5bacd5c
HB-1001 <75037684+HB-1001@users.noreply.github.com>
2022-03-07 23:45:35 +0400

    changes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,6 +59,8 @@ return packer.startup(function(use)
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
   use "Pocco81/AutoSave.nvim"
+  use "tpope/vim-endwise"
+  use "tpope/vim-rails"
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
